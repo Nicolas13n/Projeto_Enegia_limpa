6cecb32f1f10b78fd8cf1e6f4dcffdc0e7ec6166
Nicolas <nicknagildo1305@gmail.com>
2025-09-15 08:21:24 -0300

    formulário

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -17,10 +17,6 @@ elements.forEach(element => {
 });
 
 
-
-
-
-
 document.querySelectorAll('a[href^="#"]').forEach(link => {
     link.addEventListener('click', function(e) {
         const destinoId = this.getAttribute('href').replace('#', '');
@@ -36,3 +32,42 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
         }
     });
 });
+
+
+let enviar = () => {
+    let nome = document.querySelector(".inNome").value
+    let email = document.querySelector(".inEmail").value
+    let mensagem = document.querySelector(".inMensagem  ").value
+    validaEmail(email)
+    'campoVazio(nome, email, mensagem)'
+    limpar()
+}
+
+const validaEmail = (email) => {
+    for (let i = 0; i < email.length; i++) {
+        if (email[i] == "@") {
+            return
+        }
+    }
+    alert("o email não possui o caracter @")
+}
+
+const campoVazio = (c1, c2, c3) => {
+    if (c1 == "") {
+        alert("um nome esta vazio")
+
+    }
+    if (c2 == "") {
+        alert("um email esta vazio")
+
+    }
+    if (c3 == "") {
+        alert("a mensagem esta vazio")
+    }
+}
+
+const limpar = () =>{
+    document.querySelector(".inNome").value = ""
+    document.querySelector(".inEmail").value = ""
+    document.querySelector(".inMensagem").value = ""
+}
